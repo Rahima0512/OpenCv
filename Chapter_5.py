662b6e16630aa3dbb2672c68d080ff4a0a673f32
Rahima Lari <77523450+Rahima0512@users.noreply.github.com>
2021-06-28 16:39:32 +0530

    Update Chapter_5.py

--- Chapter_5.py
+++ Chapter_5.py
@@ -2,13 +2,15 @@ import cv2
 import numpy as np
 img=cv2.imread("Cards.jpg")
 
-width,height=791,1007
-pts1=np.float32([[177,337],[787,237],[943,1127],[325,1237]])
-pts2=np.float32([[0,0],[width,0],[0,height],[width,height]])
-matrix=cv2.getPerspectiveTransform(pts1,pts2)
-imgOut=cv2.warpPerspective(img,matrix,(width,height))
+width,height = 250,350
+pts1 = np.float32([[111,219],[287,188],[154,482],[352,440]])
+pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
+matrix = cv2.getPerspectiveTransform(pts1,pts2)
+imgOutput = cv2.warpPerspective(img,matrix,(width,height))
 
 cv2.imshow("image",img)
-cv2.imshow("Output",imgOut)
+cv2.imshow("Output",imgOutput)
+
+
 
 cv2.waitKey(0)
